009e753c062d69d57906e6d5d50f6267d14669ce
zarina077 <127084135+zarina077@users.noreply.github.com>
2023-03-18 14:13:58 +0300

    InputRange.file
    pink

---
+++ inputRange/app.js
@@ -0,0 +1,12 @@
+const slideValue = document.querySelector("span");
+const inputSlider = document.querySelector("input")
+inputSlider.oninput = (()=>{
+    let value = inputSlider.value;
+    slideValue.textContent = value;
+    slideValue.style.left = (value/2) + "%";
+slideValue.classList.add("show");
+})    
+inputSlider.onblur = (()=>{
+    slideValue.classList.remove("show");
+
+})
